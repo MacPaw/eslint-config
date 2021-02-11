1e492b047b3b2399b75a73a9bf6451e2c7d069ec
korovka <neporozhniy@gmail.com>
2021-02-11 17:17:14 +0200

    fix typo

--- eslint-config-base/index.js
+++ eslint-config-base/index.js
@@ -108,7 +108,7 @@ module.exports = {
     'no-lonely-if': 2,
     'no-new-object': 2,
     'no-mixed-spaces-and-tabs': 2,
-    'no-multiple-empty-lines': [2, { max: 2 }]
+    'no-multiple-empty-lines': [2, { max: 2 }],
     'no-trailing-spaces': 2,
     'no-unneeded-ternary': 2,
     'object-curly-spacing': [2, 'always'],
